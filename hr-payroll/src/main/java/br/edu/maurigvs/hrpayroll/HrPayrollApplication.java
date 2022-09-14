f1fa4fa2db95f1afa07f8d15e5cf41eb1977d3a3
Mauri Gonçalves <maurigvs@icloud.com>
2022-09-14 14:16:15 -0300

    Aula 2.12 - Project hr-payroll created

---
+++ hr-payroll/src/main/java/br/edu/maurigvs/hrpayroll/HrPayrollApplication.java
@@ -0,0 +1,13 @@
+package br.edu.maurigvs.hrpayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrPayrollApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrPayrollApplication.class, args);
+    }
+
+}
