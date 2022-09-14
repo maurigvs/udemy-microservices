0cbf0b2a5a4c60adeaf0759169c9a1ccebb65b98
Mauri Gonçalves <maurigvs@icloud.com>
2022-09-14 20:44:16 -0300

    Aula 2.22 - Project hr-api-gateway-zuul created

---
+++ hr-api-gateway-zuul/src/test/java/br/edu/maurigvs/hrapigatewayzuul/HrApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.maurigvs.hrapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
