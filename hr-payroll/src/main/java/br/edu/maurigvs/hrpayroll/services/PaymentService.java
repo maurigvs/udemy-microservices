9eaeb89b63902739f478cc49d104b5d01f153b99
Mauri Gonçalves <maurigvs@icloud.com>
2022-09-14 14:42:00 -0300

    Aula 2.14 - RestTemplate

--- hr-payroll/src/main/java/br/edu/maurigvs/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/edu/maurigvs/hrpayroll/services/PaymentService.java
@@ -1,13 +1,29 @@
 package br.edu.maurigvs.hrpayroll.services;
 
 import br.edu.maurigvs.hrpayroll.entities.Payment;
+import br.edu.maurigvs.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Payment getPayment(long workerId, int days){
-        // Mock
-        return new Payment("Bob", 100.0, days);
+
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", ""+workerId);
+        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+        return new Payment(worker.getName(),worker.getDailyIncome(), days);
     }
 }
