f6f7de4e194b40218be76b1a8a3332d85d1c306d
Mauri Gonçalves <maurigvs@icloud.com>
2022-09-15 14:37:07 -0300

    Aula 4.35 - Database Seeding

--- hr-user/src/main/java/br/edu/maurigvs/hruser/entities/User.java
+++ hr-user/src/main/java/br/edu/maurigvs/hruser/entities/User.java
@@ -20,7 +20,7 @@ public class User implements Serializable {
     private String password;
 
     @ManyToMany(fetch = FetchType.EAGER) // load Role data along with User data (opposite of .LAZY)
-    @JoinTable(name = "user_role",
+    @JoinTable(name = "users_roles",
             joinColumns = @JoinColumn(name= "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
