d04f38d15633fd8a9749cc80e3b39f6ff2cf44d0
Mauri Gonçalves <maurigvs@icloud.com>
2022-09-15 14:29:26 -0300

    Aula 4.34 - User Role many-to-many association

---
+++ hr-user/src/main/java/br/edu/maurigvs/hruser/entities/Role.java
@@ -0,0 +1,52 @@
+package br.edu.maurigvs.hruser.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "roles")
+public class Role implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String roleName;
+
+    public Role() {
+    }
+
+    public Role(Long id, String roleName) {
+        this.id = id;
+        this.roleName = roleName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Role)) return false;
+        Role role = (Role) o;
+        return Objects.equals(getRoleName(), role.getRoleName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getRoleName());
+    }
+}
