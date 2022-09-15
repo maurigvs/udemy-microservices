dca762810a8ee55b317e0969973997cb193ff320
Mauri Gonçalves <maurigvs@icloud.com>
2022-09-15 14:57:25 -0300

    Aula 4.37 - Project hr-oauth created

---
+++ hr-oauth/src/main/java/br/edu/maurigvs/hroauth/HrOauthApplication.java
@@ -0,0 +1,13 @@
+package br.edu.maurigvs.hroauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrOauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrOauthApplication.class, args);
+    }
+
+}
---
+++ hr-oauth/src/test/java/br/edu/maurigvs/hroauth/HrOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.maurigvs.hroauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
